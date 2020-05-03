3bcd1a2549d9a632f150a778b6b2e48674d14912
MikhailUsachev-Piter <mikhail.usachev@gmail.com>
2020-05-03 16:15:35 +0300

    Added Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,14 @@
 public class Main {
+    public static void main(String[] args) {
+        int account = 30000; // Копейки
+        int admission = 20000; // Копейки
+        if (admission >= 20000){
+            int bonus = admission / 10000;
+            account = bonus + ((account + admission) / 100);
+            System.out.println(account);
+        }   else {
+            account = (account + admission) / 100;
+            System.out.println(account);
+        }
+    }
 }
