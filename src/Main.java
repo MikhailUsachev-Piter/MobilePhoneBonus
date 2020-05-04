454400ad5a0bd58066229720bec4aeda78659088
Mikhail Usachev <63136175+MikhailUsachev-Piter@users.noreply.github.com>
2020-05-04 12:36:38 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        int payment = 1500_00; // В копейках
+        int payment = 200_00; // В копейках
         if (payment >= 1000_00){
             int bonus = payment / 100_00;
+            System.out.println(bonus);
+        } else {
+            int bonus = 0;
             System.out.println(bonus);
         }
     }
